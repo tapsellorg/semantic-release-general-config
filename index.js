2c2b6aa8e3e042cc5056fba4e8523aae7c19c61b
Vahid Mohammadi <vahidm1467@gmail.com>
2022-08-15 19:24:29 +0430

    feat: add tapsell shared semantic release config

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'improve', release: 'patch' },
+          { type: 'perf', release: 'major' },
+          { breaking: true, release: 'minor' },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features & Improvements' },
+            { type: 'improve', section: 'Features & Improvements' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'style', section: 'Style Changes', hidden: true },
+          ],
+        },
+        linkReferences: false,
+        writerOpts: {},
+      },
+    ],
+    '@semantic-release/changelog',
     '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore: release <%= nextRelease.version %> [skip ci]',
+        assets: ['package.json', 'CHANGELOG.md'],
+      },
+    ],
     '@semantic-release/gitlab',
   ],
+  branches: ['master', { name: 'dev', prerelease: true }],
 };

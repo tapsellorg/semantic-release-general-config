0aac2d8e47bf9f2e92b906a64a7b240c8af34b1f
Vahid Mohammadi <vahidm1467@gmail.com>
2022-09-07 18:18:47 +0430

    chore: add next version to a log file for usage in other tools

--- index.js
+++ index.js
@@ -1,49 +1,55 @@
 module.exports = {
   plugins: [
     [
-      '@semantic-release/commit-analyzer',
+      "@semantic-release/commit-analyzer",
       {
-        preset: 'angular',
+        preset: "angular",
         releaseRules: [
-          { type: 'feat', release: 'minor' },
-          { type: 'fix', release: 'patch' },
-          { type: 'refactor', release: 'patch' },
-          { type: 'style', release: 'patch' },
-          { type: 'improve', release: 'patch' },
-          { type: 'perf', release: 'major' },
-          { breaking: true, release: 'minor' },
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "improve", release: "patch" },
+          { type: "perf", release: "major" },
+          { breaking: true, release: "minor" },
         ],
         parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
         },
       },
     ],
     [
-      '@semantic-release/release-notes-generator',
+      "@semantic-release/release-notes-generator",
       {
-        preset: 'conventionalcommits',
+        preset: "conventionalcommits",
         presetConfig: {
           types: [
-            { type: 'feat', section: 'Features & Improvements' },
-            { type: 'improve', section: 'Features & Improvements' },
-            { type: 'fix', section: 'Bug Fixes' },
-            { type: 'style', section: 'Style Changes', hidden: true },
+            { type: "feat", section: "Features & Improvements" },
+            { type: "improve", section: "Features & Improvements" },
+            { type: "fix", section: "Bug Fixes" },
+            { type: "style", section: "Style Changes", hidden: true },
           ],
         },
         linkReferences: false,
         writerOpts: {},
       },
     ],
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
     [
-      '@semantic-release/git',
+      "@semantic-release/exec",
       {
-        message: 'chore: release <%= nextRelease.version %> [skip ci]',
-        assets: ['package.json', 'CHANGELOG.md'],
+        prepareCmd: "echo ${nextRelease.version} > release-next-version.log",
       },
     ],
-    '@semantic-release/gitlab',
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        message: "chore: release <%= nextRelease.version %> [skip ci]",
+        assets: ["package.json", "CHANGELOG.md"],
+      },
+    ],
+    "@semantic-release/gitlab",
   ],
-  branches: ['master', { name: 'dev', prerelease: true }],
+  branches: ["master", { name: "dev", prerelease: true }],
 };
